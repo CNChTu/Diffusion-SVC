664c5961cf4a7035e926d3a19fd3ab82a02170e3
ylzz1997 <ylzz1997@outlook.com>
2023-09-06 02:37:02 +0800

    Debug

--- draw.py
+++ draw.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import random
 from tqdm import tqdm
+import shutil
 
 def parse_args(args=None, namespace=None):
     """Parse command-line arguments."""
@@ -30,25 +31,24 @@ if __name__ == '__main__':
     train_rate = cmd.trainrate
     test_rate = 1 - train_rate
 
-    for i in os.listdir('./dataset_raw'):
+    for i in list(filter(lambda x: os.path.isdir(f"./dataset_raw/{x}") ,os.listdir('./dataset_raw'))):
         speaker_path = os.path.join('./dataset_raw', i)
         # 读取dataset_raw文件夹下的所有.wav文件
-        file_list = list(filter(lambda x: x.endswith('.wav') ,os.listdir('./dataset_raw')))
-        file_list = list(map(lambda x: os.path.join("./dataset_raw",x), file_list))
+        file_list = list(filter(lambda x: x.endswith('.wav') ,os.listdir(speaker_path)))
+        file_list = list(map(lambda x: os.path.join("./dataset_raw",i,x), file_list))
         
         # 打乱文件列表
-        file_list = random.shuffle(file_list)
-    
+        random.shuffle(file_list)
         train_list = file_list[:int(len(file_list)*train_rate)]
         val_list = file_list[int(len(file_list)*train_rate):]
     
-        train_path = f"./dataset/train/audio/{i}"
+        train_path = f"./data/train/audio/{i}/"
         mkdir(train_path)
-        val_path = f"./dataset/val/audio/{i}"
+        val_path = f"./data/val/audio/{i}/"
         mkdir(val_path)
 
-        for j in tqdm(train_list, prefix=f"copying {i} train set"):
-            os.system(f"cp {j} {train_path}")
-        for j in tqdm(val_list, prefix=f"copying {i} val set"):
-            os.system(f"cp {j} {val_path}")
+        for j in tqdm(train_list, desc=f"copying {i} train set"):
+            shutil.copy(j, train_path)
+        for j in tqdm(val_list, desc=f"copying {i} val set"):
+            shutil.copy(j, val_path)
             
