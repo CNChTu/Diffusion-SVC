215bc39dae99b558c28d1117973dabde378c61a2
CN_ChiTu <36254426+CNChTu@users.noreply.github.com>
2023-09-26 22:18:31 +0800

    add diffusion v2

--- diffusion/unit2mel.py
+++ diffusion/unit2mel.py
@@ -7,6 +7,7 @@ import torch.nn.functional as F
 from torch.nn.utils import weight_norm
 from .diffusion import GaussianDiffusion
 from .wavenet import WaveNet
+from .convnext import ConvNext
 from .vocoder import Vocoder
 from .naive.naive import Unit2MelNaive
 
@@ -18,6 +19,18 @@ class DotDict(dict):
 
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
+
+
+def get_z(stack_tensor, mean_only=False):
+    # stack_tensor: [B x N x D x 2]
+    # sample z, or mean only
+    m = stack_tensor.transpose(-1, 0)[:1].transpose(-1, 0).squeeze(-1)
+    logs = stack_tensor.transpose(-1, 0)[:1].transpose(-1, 0).squeeze(-1)
+    if mean_only:
+        z = m  # mean only
+    else:
+        z = m + torch.randn_like(m) * torch.exp(logs)  # sample z
+    return z  # [B x N x D]
 
 
 def load_model_vocoder(
@@ -72,33 +85,42 @@ def load_model_vocoder_from_combo(combo_model_path, device='cpu'):
 def load_svc_model(args, vocoder_dimension):
     if args.model.type == 'Diffusion':
         model = Unit2Mel(
-                    args.data.encoder_out_channels,
-                    args.model.n_spk,
-                    args.model.use_pitch_aug,
-                    vocoder_dimension,
-                    args.model.n_layers,
-                    args.model.n_chans,
-                    args.model.n_hidden,
-                    use_speaker_encoder=args.model.use_speaker_encoder,
-                    speaker_encoder_out_channels=args.data.speaker_encoder_out_channels,
-                    z_rate=args.model.z_rate,
-                    mean_only=args.model.mean_only,
-                    wn_dilation=args.model.wn_dilation,
-                    max_beta=args.model.max_beta,
-                    spec_min=args.model.spec_min,
-                    spec_max=args.model.spec_max,
-                    wn_kernel=args.model.wn_kernel)
+            args.data.encoder_out_channels,
+            args.model.n_spk,
+            args.model.use_pitch_aug,
+            vocoder_dimension,
+            args.model.n_layers,
+            args.model.n_chans,
+            args.model.n_hidden,
+            use_speaker_encoder=args.model.use_speaker_encoder,
+            speaker_encoder_out_channels=args.data.speaker_encoder_out_channels)
+
+    elif args.model.type == 'DiffusionV2':
+        model = Unit2MelV2(
+            args.data.encoder_out_channels,
+            args.model.n_spk,
+            args.model.use_pitch_aug,
+            vocoder_dimension,
+            args.model.n_hidden,
+            use_speaker_encoder=args.model.use_speaker_encoder,
+            speaker_encoder_out_channels=args.data.speaker_encoder_out_channels,
+            z_rate=args.model.z_rate,
+            mean_only=args.model.mean_only,
+            max_beta=args.model.max_beta,
+            spec_min=args.model.spec_min,
+            spec_max=args.model.spec_max,
+            denoise_fn=args.model.denoise_fn)
 
     elif args.model.type == 'Naive':
         model = Unit2MelNaive(
-                args.data.encoder_out_channels,
-                args.model.n_spk,
-                args.model.use_pitch_aug,
-                vocoder_dimension,
-                args.model.n_layers,
-                args.model.n_chans,
-                use_speaker_encoder=args.model.use_speaker_encoder,
-                speaker_encoder_out_channels=args.data.speaker_encoder_out_channels)
+            args.data.encoder_out_channels,
+            args.model.n_spk,
+            args.model.use_pitch_aug,
+            vocoder_dimension,
+            args.model.n_layers,
+            args.model.n_chans,
+            use_speaker_encoder=args.model.use_speaker_encoder,
+            speaker_encoder_out_channels=args.data.speaker_encoder_out_channels)
 
     elif args.model.type == 'NaiveFS':
         model = Unit2MelNaive(
@@ -112,12 +134,156 @@ def load_svc_model(args, vocoder_dimension):
             speaker_encoder_out_channels=args.data.speaker_encoder_out_channels,
             use_full_siren=True,
             l2reg_loss=args.model.l2_reg_loss)
+
     else:
-        raise ("Unknow model")
+        raise TypeError(" [X] Unknow model")
     return model
 
 
+class Unit2MelV2(nn.Module):
+    def __init__(
+            self,
+            input_channel,
+            n_spk,
+            use_pitch_aug=False,
+            out_dims=128,
+            n_hidden=256,
+            use_speaker_encoder=False,
+            speaker_encoder_out_channels=256,
+            z_rate=None,
+            mean_only=False,
+            max_beta=0.02,
+            spec_min=-12,
+            spec_max=2,
+            denoise_fn=None):
+        super().__init__()
+
+        if denoise_fn is None:
+            # catch None
+            denoise_fn = {'type': 'WaveNet',
+                          'wn_layers': 20,
+                          'wn_chans': 384,
+                          'wn_dilation': 1,
+                          'wn_kernel': 3,
+                          'wn_tf_use': False,
+                          'wn_tf_n_layers': 2,
+                          'wn_tf_n_head': 4}
+            denoise_fn = DotDict(denoise_fn)
+
+        if denoise_fn.type == 'WaveNet':
+            # catch None
+            self.wn_layers = denoise_fn.wn_layers if (denoise_fn.wn_layers is not None) else 20
+            self.wn_chans = denoise_fn.wn_chans if (denoise_fn.wn_chans is not None) else 384
+            self.wn_dilation = denoise_fn.wn_dilation if (denoise_fn.wn_dilation is not None) else 1
+            self.wn_kernel = denoise_fn.wn_kernel if (denoise_fn.wn_kernel is not None) else 3
+            self.wn_tf_use = denoise_fn.wn_tf_use if (denoise_fn.wn_tf_use is not None) else False
+            self.wn_tf_n_layers = denoise_fn.wn_tf_n_layers if (denoise_fn.wn_tf_n_layers is not None) else 2
+            self.wn_tf_n_head = denoise_fn.wn_tf_n_head if (denoise_fn.wn_tf_n_head is not None) else 4
+            # init wavenet denoiser
+            denoiser = WaveNet(out_dims, self.wn_layers, self.wn_chans, n_hidden, self.wn_dilation, self.wn_kernel,
+                               self.wn_tf_use, self.wn_tf_n_layers, self.wn_tf_n_head)
+
+        elif denoise_fn.type == 'ConvNext':
+            # catch None
+            self.cn_layers = denoise_fn.cn_layers if (denoise_fn.cn_layers is not None) else 20
+            self.cn_chans = denoise_fn.cn_chans if (denoise_fn.cn_chans is not None) else 384
+            self.cn_dilation_cycle = denoise_fn.cn_dilation_cycle if (denoise_fn.cn_dilation_cycle is not None) else 4
+            self.mlp_factor = denoise_fn.mlp_factor if (denoise_fn.mlp_factor is not None) else 4
+            # init convnext denoiser
+            denoiser = ConvNext(
+                mel_channels=out_dims,
+                dim=self.cn_chans,
+                mlp_factor=self.mlp_factor,
+                condition_dim=n_hidden,
+                num_layers=self.cn_layers,
+                dilation_cycle=self.cn_dilation_cycle,
+            )
+
+        else:
+            raise TypeError(" [X] Unknow denoise_fn")
+
+        # catch None
+        self.z_rate = z_rate
+        self.mean_only = mean_only if (mean_only is not None) else False
+        self.max_beta = max_beta if (max_beta is not None) else 0.02
+        self.spec_min = spec_min if (spec_min is not None) else -12
+        self.spec_max = spec_max if (spec_max is not None) else 2
+
+        # init embed
+        self.unit_embed = nn.Linear(input_channel, n_hidden)
+        self.f0_embed = nn.Linear(1, n_hidden)
+        self.volume_embed = nn.Linear(1, n_hidden)
+        if use_pitch_aug:
+            self.aug_shift_embed = nn.Linear(1, n_hidden, bias=False)
+        else:
+            self.aug_shift_embed = None
+        self.n_spk = n_spk
+        self.use_speaker_encoder = use_speaker_encoder
+        if use_speaker_encoder:
+            self.spk_embed = nn.Linear(speaker_encoder_out_channels, n_hidden, bias=False)
+        else:
+            if n_spk is not None and n_spk > 1:
+                self.spk_embed = nn.Embedding(n_spk, n_hidden)
+
+        # init diffusion
+        self.decoder = GaussianDiffusion(
+            denoiser,
+            out_dims=out_dims,
+            max_beta=self.max_beta,
+            spec_min=self.spec_min,
+            spec_max=self.spec_max)
+
+    def forward(self, units, f0, volume, spk_id=None, spk_mix_dict=None, aug_shift=None,
+                gt_spec=None, infer=True, infer_speedup=10, method='dpm-solver', k_step=None, use_tqdm=True,
+                spk_emb=None, spk_emb_dict=None, use_vae=False):
+        '''
+        input: 
+            B x n_frames x n_unit
+        return: 
+            dict of B x n_frames x feat
+        '''
+
+        # embed
+        x = self.unit_embed(units) + self.f0_embed((1 + f0 / 700).log()) + self.volume_embed(volume)
+        if self.use_speaker_encoder:
+            if spk_mix_dict is not None:
+                assert spk_emb_dict is not None
+                for k, v in spk_mix_dict.items():
+                    spk_id_torch = spk_emb_dict[str(k)]
+                    spk_id_torch = np.tile(spk_id_torch, (len(units), 1))
+                    spk_id_torch = torch.from_numpy(spk_id_torch).float().to(units.device)
+                    x = x + v * self.spk_embed(spk_id_torch)
+            else:
+                x = x + self.spk_embed(spk_emb)
+        else:
+            if self.n_spk is not None and self.n_spk > 1:
+                if spk_mix_dict is not None:
+                    for k, v in spk_mix_dict.items():
+                        spk_id_torch = torch.LongTensor(np.array([[k]])).to(units.device)
+                        x = x + v * self.spk_embed(spk_id_torch - 1)
+                else:
+                    x = x + self.spk_embed(spk_id - 1)
+        if self.aug_shift_embed is not None and aug_shift is not None:
+            x = x + self.aug_shift_embed(aug_shift / 5)
+
+        # sample z or mean only
+        if use_vae and (gt_spec is not None):
+            gt_spec = get_z(gt_spec, mean_only=self.mean_only)
+            if (self.z_rate is not None) and (self.z_rate != 0):
+                gt_spec = gt_spec * self.z_rate  # scale z
+
+        # diffusion
+        x = self.decoder(x, gt_spec=gt_spec, infer=infer, infer_speedup=infer_speedup, method=method, k_step=k_step,
+                         use_tqdm=use_tqdm)
+
+        if (self.z_rate is not None) and (self.z_rate != 0):
+            x = x / self.z_rate  # scale z
+
+        return x
+
+
 class Unit2Mel(nn.Module):
+    # old version
     def __init__(
             self,
             input_channel,
@@ -128,23 +294,8 @@ class Unit2Mel(nn.Module):
             n_chans=384,
             n_hidden=256,
             use_speaker_encoder=False,
-            speaker_encoder_out_channels=256,
-            z_rate=None,
-            mean_only=False,
-            wn_dilation=1,
-            max_beta=0.02,
-            spec_min=-12,
-            spec_max=2,
-            wn_kernel=3):
+            speaker_encoder_out_channels=256):
         super().__init__()
-        self.z_rate = z_rate
-        self.mean_only = mean_only if (mean_only is not None) else False
-        self.wn_dilation = wn_dilation if (wn_dilation is not None) else 1
-        self.max_beta = max_beta if (max_beta is not None) else 0.02
-        self.spec_min = spec_min if (spec_min is not None) else -12
-        self.spec_max = spec_max if (spec_max is not None) else 2
-        self.wn_kernel = wn_kernel if (wn_kernel is not None) else 3
-
         self.unit_embed = nn.Linear(input_channel, n_hidden)
         self.f0_embed = nn.Linear(1, n_hidden)
         self.volume_embed = nn.Linear(1, n_hidden)
@@ -161,20 +312,17 @@ class Unit2Mel(nn.Module):
                 self.spk_embed = nn.Embedding(n_spk, n_hidden)
 
         # diffusion
-        self.decoder = GaussianDiffusion(WaveNet(out_dims, n_layers, n_chans, n_hidden, self.wn_dilation, self.wn_kernel),
-                                         out_dims=out_dims,
-                                         max_beta=self.max_beta,
-                                         spec_min=self.spec_min,
-                                         spec_max=self.spec_max)
+        self.decoder = GaussianDiffusion(WaveNet(out_dims, n_layers, n_chans, n_hidden, 1, 3, False),
+                                         out_dims=out_dims, max_beta=0.02, spec_min=-12, spec_max=2)
 
     def forward(self, units, f0, volume, spk_id=None, spk_mix_dict=None, aug_shift=None,
                 gt_spec=None, infer=True, infer_speedup=10, method='dpm-solver', k_step=None, use_tqdm=True,
-                spk_emb=None, spk_emb_dict=None, use_vae=False):
+                spk_emb=None, spk_emb_dict=None):
 
         '''
-        input: 
+        input:
             B x n_frames x n_unit
-        return: 
+        return:
             dict of B x n_frames x feat
         '''
 
@@ -200,25 +348,7 @@ class Unit2Mel(nn.Module):
         if self.aug_shift_embed is not None and aug_shift is not None:
             x = x + self.aug_shift_embed(aug_shift / 5)
 
-        if use_vae and (gt_spec is not None):
-            gt_spec = get_z(gt_spec, mean_only=self.mean_only)
-            if (self.z_rate is not None) and (self.z_rate != 0):
-                gt_spec = gt_spec * self.z_rate
-
         x = self.decoder(x, gt_spec=gt_spec, infer=infer, infer_speedup=infer_speedup, method=method, k_step=k_step,
                          use_tqdm=use_tqdm)
 
-        if (self.z_rate is not None) and (self.z_rate != 0):
-            x = x / self.z_rate
-
         return x
-
-
-def get_z(stack_tensor, mean_only=False):
-    m = stack_tensor.transpose(-1, 0)[:1].transpose(-1, 0).squeeze(-1)
-    logs = stack_tensor.transpose(-1, 0)[:1].transpose(-1, 0).squeeze(-1)
-    if mean_only:
-        z = m
-    else:
-        z = m + torch.randn_like(m) * torch.exp(logs)
-    return z
--- diffusion/wavenet.py
+++ diffusion/wavenet.py
@@ -62,7 +62,8 @@ class ResidualBlock(nn.Module):
 
 
 class WaveNet(nn.Module):
-    def __init__(self, in_dims=128, n_layers=20, n_chans=384, n_hidden=256, dilation=1, kernel_size=3):
+    def __init__(self, in_dims=128, n_layers=20, n_chans=384, n_hidden=256, dilation=1, kernel_size=3,
+                 transformer_use=False, transformer_n_layers=2, transformer_n_head=4):
         super().__init__()
         self.input_projection = Conv1d(in_dims, n_chans, 1)
         self.diffusion_embedding = SinusoidalPosEmb(n_chans)
@@ -80,6 +81,17 @@ class WaveNet(nn.Module):
             )
             for i in range(n_layers)
         ])
+        if transformer_use:
+            transformer_layer = nn.TransformerEncoderLayer(
+                d_model=n_chans,
+                nhead=transformer_n_head,
+                dim_feedforward=n_chans * 4,
+                dropout=0.1,
+                activation='gelu'
+            )
+            self.transformer = nn.TransformerEncoder(transformer_layer, num_layers=transformer_n_layers)
+        else:
+            self.transformer = None
         self.skip_projection = Conv1d(n_chans, n_chans, 1)
         self.output_projection = Conv1d(n_chans, in_dims, 1)
         nn.init.zeros_(self.output_projection.weight)
@@ -105,5 +117,7 @@ class WaveNet(nn.Module):
         x = torch.sum(torch.stack(skip), dim=0) / sqrt(len(self.residual_layers))
         x = self.skip_projection(x)
         x = F.relu(x)
+        if self.transformer is not None:
+            x = self.transformer(x.transpose(1, 2)).transpose(1, 2)
         x = self.output_projection(x)  # [B, mel_bins, T]
         return x[:, None, :, :]
