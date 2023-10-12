9d0c5919c4bb12ca6edccd922cbebfe1661f6ecb
ylzz1997 <ylzz1997@outlook.com>
2023-10-13 01:10:11 +0800

    updata

--- text2semantic/roformer/roformer.py
+++ text2semantic/roformer/roformer.py
@@ -1,4 +1,4 @@
-from transformers import LlamaForCausalLM, LlamaConfig
+from transformers import RoFormerForCausalLM, RoFormerModel, RoFormerConfig
 import torch
 from torch import nn
 from text.symbols import *
@@ -8,10 +8,10 @@ from cluster import get_cluster_model, get_cluster_result, get_cluster_center_re
 from torch.nn.utils.rnn import pad_sequence, pack_sequence
 
 
-class Llama(nn.Module):
+class Roformer(nn.Module):
     def __init__(
         self,
-        config: LlamaConfig,
+        config: RoFormerConfig,
         mode = "phone",
         semantic_kmeans_num = 10000,
         codebook_path = "pretrain/semantic_codebook.pt",
@@ -21,20 +21,34 @@ class Llama(nn.Module):
         self.config = config
         if "phone" in self.mode:
             token_size = len(symbols)
-            token_size += semantic_kmeans_num + num_tones
-            self.PAD = pad_id
+            # token_size += semantic_kmeans_num + num_tones
             self.BOS = token_size
-            self.SEQ = token_size + 1
-            self.EOS = token_size + 2
+            self.EOS = token_size + 1
+            self.PAD = token_size + 2
             token_size += 3
             # self.tone_emb = nn.Embedding(num_tones, config.hidden_size)
             # self.phone_emb = nn.Embedding(token_size + 2, config.hidden_size)
         config.vocab_size = token_size
-        self.llama = LlamaForCausalLM(config)
+        config.type_vocab_size = num_tones + 1
+        config.pad_token_id = self.PAD
+        config.bos_token_id = self.BOS
+        config.eos_token_id = self.EOS
+        self.text_encoder = RoFormerModel(config)
+
+        config.bos_token_id = semantic_kmeans_num
+        config.eos_token_id = semantic_kmeans_num + 1
+        config.pad_token_id = semantic_kmeans_num + 2
+        config.vocab_size = semantic_kmeans_num + 3
+
+        config.type_vocab_size = 1
+        config.is_decoder = True
+        config.add_cross_attention = True
+        self.semantic_decoder = RoFormerForCausalLM(config)
+        
         self.quantizer = get_cluster_model(codebook_path)
 
-        if self.llama.model.embed_tokens.weight.data.shape[1] == self.quantizer.cluster_centers_.shape[1]:
-            self.llama.model.embed_tokens.weight.data[len(symbols) - 1:len(symbols) + semantic_kmeans_num - 1] = torch.from_numpy(self.quantizer.cluster_centers_.copy())
+        if self.semantic_decoder.roformer.embeddings.word_embeddings.weight.data.shape[1] == self.quantizer.cluster_centers_.shape[1]:
+            self.semantic_decoder.roformer.embeddings.word_embeddings.weight.data[:semantic_kmeans_num] = torch.from_numpy(self.quantizer.cluster_centers_.copy())
 
 
     def forward(
@@ -42,12 +56,8 @@ class Llama(nn.Module):
         phone,
         tone,
         semantic,
-        input_ids=None,
         attention_mask=None,
-        token_type_ids=None,
-        position_ids=None,
-        head_mask=None,
-        inputs_embeds=None,
+        encoder_attention_mask=None,
         labels=None,
         use_cache=None,
         output_attentions=None,
@@ -55,50 +65,41 @@ class Llama(nn.Module):
         return_dict=None,
         **kwargs
     ):
-        if input_ids == None:
-            B,T = phone.shape
-            phone_emb = self.phone_emb(phone)
-            tone_emb = self.tone_emb(tone)
-            seq_emb = self.phone_emb(torch.ones((B, 1),device=phone.device) * self.SEQ)
-            bos_emb = self.phone_emb(torch.ones((B, 1),device=phone.device) * self.BOS)
-            eos_emb = self.llama.model.embed_tokens(torch.ones_like(phone[:,0:1]) * self.EOS)
-            phone_tone_emb = phone_emb + tone_emb
+        B,T = phone.shape
+        phone_tone_emb = self.text_encoder.embeddings(phone,tone)
 
-            phone_tone_emb = torch.cat([bos_emb, phone_tone_emb, seq_emb], dim=1)
+        encoder_hidden_states = self.text_encoder(
+            inputs_embeds = phone_tone_emb,
+            attention_mask = encoder_attention_mask,
+            use_cache = use_cache
+        )[0]
 
-            semantic_emb = self.llama.model.embed_tokens(semantic)
-            semantic_emb = torch.cat([semantic_emb, eos_emb], dim=1)
-
-            inputs_embeds = torch.cat([phone_tone_emb, semantic_emb], dim=1)
-        else:
-            outputs = self.llama(
-                attention_mask=attention_mask,
-                position_ids=position_ids,
-                past_key_values=None,
-                inputs_embeds=inputs_embeds,
-                use_cache=use_cache,
-                labels=labels,
-                output_attentions=output_attentions,
-                output_hidden_states=output_hidden_states,
-                return_dict=return_dict,
-            )
-            
-
+        outputs = self.semantic_decoder(
+            semantic,
+            encoder_hidden_states = encoder_hidden_states,
+            encoder_attention_mask = encoder_attention_mask,
+            attention_mask = attention_mask,
+            labels = labels,
+            output_attentions = output_attentions,
+            output_hidden_states = output_hidden_states,
+            return_dict = return_dict,
+            use_cache = use_cache
+        )
         return outputs
 
 if __name__ == '__main__':
-    a = LlamaConfig(
+    a = RoFormerConfig(
          hidden_size=768,
             num_attention_heads=4,
             num_hidden_layers=4,
             num_hidden_groups=1,
             intermediate_size=512,
     )
-    b = Llama(config=a)
+    b = Roformer(config=a)
     phone = torch.LongTensor([[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]])
     tone = torch.LongTensor([[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]])
     semantic = torch.LongTensor([[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]])
-    labels = torch.LongTensor([[1,2,3,4,5,6,7,8,9,10,11,1,2,3,4,5,6,7,8,9,10,11,1,2,3,8,9,10,11,-100,-100,-100,-100]])
+    labels = torch.LongTensor([[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]])
     outputs = b(phone=phone, tone=tone, semantic=semantic,labels=labels)
     print(outputs)
 
