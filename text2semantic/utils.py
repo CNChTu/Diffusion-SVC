41196d1c16510cf6ff79fa633bd9a0c790fee5f1
ylzz1997 <ylzz1997@outlook.com>
2023-10-20 01:11:59 +0800

    debug

--- text2semantic/utils.py
+++ text2semantic/utils.py
@@ -12,3 +12,9 @@ def get_data_loaders(args, model, accelerate = None):
     
     return loader_train, loader_val
 
+@torch.no_grad()
+def get_topk_acc(gt_token, logist, k = 5):
+    _, topk = torch.topk(logist, k, dim=-1)
+    topk = topk.cpu().numpy()
+    gt_token = gt_token.cpu().numpy()
+    return sum([1 if gt_token[i] in topk[i] else 0 for i in range(len(gt_token))]) / len(gt_token)
