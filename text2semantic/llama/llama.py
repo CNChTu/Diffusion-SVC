Fix output calculation in Llama class

text2semantic/llama/llama.py
@@ -224,9 +224,9 @@ class Llama(nn.Module):
             logits_processor = logits_processor
         )
 
-        outputs = outputs[:, input_ids.shape[1]:] - self.semantic_token_shift
         if (outputs >= self.config.bos_token_id).any():
             outputs[outputs >= self.config.bos_token_id] = self.config.bos_token_id - 1
+        outputs = outputs[:, input_ids.shape[1]:] - self.semantic_token_shift
 
         return outputs
 if __name__ == '__main__':