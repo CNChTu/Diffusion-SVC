Fix attention score calculation in RoFormerSelfAttention

text2semantic/roformer/roformer_model.py
@@ -304,7 +304,7 @@ class RoFormerSelfAttention(nn.Module):
         # Take the dot product between "query" and "key" to get the raw attention scores.
         attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
         
-        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        # attention_scores = attention_scores / math.sqrt(self.attention_head_size)
         if attention_mask is not None:
             # Apply the attention mask is (precomputed for all layers in RoFormerModel forward() function)
             attention_scores = attention_scores + attention_mask