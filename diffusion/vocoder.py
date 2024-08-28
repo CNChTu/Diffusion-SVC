0dec3c53aa631c55d34bef62012f2178d2dc8fdc
CN_ChiTu <36254426+CNChTu@users.noreply.github.com>
2024-08-29 02:13:16 +0800

    add dct, evagan vocoder

--- diffusion/vocoder.py
+++ diffusion/vocoder.py
@@ -7,6 +7,9 @@ from encoder.hifi_vaegan import InferModel
 import json
 import yaml
 from encoder.fireflygan import FireflyBase
+from encoder.evagan import EVAGANBase as EVABase
+from encoder.evagan import EVAGANBig as EVABig
+from encoder.dct.dct import DCT, IDCT
 
 
 def load_vocoder_for_save(vocoder_type, model_path, device='cpu'):
@@ -18,6 +21,12 @@ def load_vocoder_for_save(vocoder_type, model_path, device='cpu'):
         vocoder = HiFiVAEGAN(model_path, device=device)
     elif vocoder_type == 'fireflygan-base':
         vocoder = FireFlyGANBase(model_path, device=device)
+    elif vocoder_type == 'evagan-base':
+        vocoder = EVAGANBase(model_path, device=device)
+    elif vocoder_type == 'evagan-big':
+        vocoder = EVAGANBig(model_path, device=device)
+    elif vocoder_type == 'dct512':
+        vocoder = DCT512(model_path, device=device)
     else:
         raise ValueError(f" [x] Unknown vocoder: {vocoder_type}")
     out_dict = vocoder.load_model_for_combo(model_path=model_path)
@@ -44,6 +53,12 @@ class Vocoder:
             self.vocoder = HiFiVAEGAN(vocoder_ckpt, device=device)
         elif vocoder_type == 'fireflygan-base':
             self.vocoder = FireFlyGANBase(vocoder_ckpt, device=device)
+        elif vocoder_type == 'evagan-base':
+            self.vocoder = EVAGANBase(vocoder_ckpt, device=device)
+        elif vocoder_type == 'evagan-big':
+            self.vocoder = EVAGANBig(vocoder_ckpt, device=device)
+        elif vocoder_type == 'dct512':
+            self.vocoder = DCT512(vocoder_ckpt, device=device)
         else:
             raise ValueError(f" [x] Unknown vocoder: {vocoder_type}")
 
@@ -72,6 +87,49 @@ class Vocoder:
         f0 = f0[:, :mel.size(1), 0]  # B, n_frames
         audio = self.vocoder(mel, f0)
         return audio
+
+
+class DCT512(torch.nn.Module):
+    def __init__(self, model_path, device=None):
+        super().__init__()
+        if device is None:
+            device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.device = device
+        self.sampling_rate = 44100,
+        self.num_mels = 512,
+        self.hop_size = 512,
+        self.dct = DCT(512)
+        self.idct = IDCT(512)
+
+    def sample_rate(self):
+        return self.sampling_rate
+
+    def hop_size(self):
+        return self.hop_size
+
+    def dimension(self):
+        return self.num_mels
+
+    def extract(self, audio, keyshift=0):
+        assert keyshift == 0
+        with torch.no_grad():
+            audio = audio.to(self.device)
+            mel = self.dct(audio)  # B, n_frames, bins
+        return mel
+
+    def forward(self, mel, f0):  # mel: B, n_frames, bins; f0: B, n_frames
+        with torch.no_grad():
+            audio = self.idct(mel)
+            return audio.unsqueeze(1)  # B, 1, T
+
+    def load_model_for_combo(self, model_path=None, device='cpu'):
+        config = {"sampling_rate": self.sampling_rate, "num_mels": self.num_mels, "hop_size": self.hop_size}
+        model = NothingFlag()
+        out_dict = {
+            "config": config,
+            "model": model
+        }
+        return out_dict
 
 
 class NsfHifiGAN(torch.nn.Module):
@@ -112,14 +170,14 @@ class NsfHifiGAN(torch.nn.Module):
         mel = self.stft.get_mel(audio, keyshift=keyshift).transpose(1, 2)  # B, n_frames, bins
         return mel
 
-    def forward(self, mel, f0):
+    def forward(self, mel, f0):  # mel: B, n_frames, bins; f0: B, n_frames
         if self.model is None:
             print('| Load HifiGAN: ', self.model_path)
             self.model, self.h = load_model(self.model_path, device=self.device)
         with torch.no_grad():
             c = mel.transpose(1, 2)
             audio = self.model(c, f0)
-            return audio
+            return audio  # B, 1, T
 
     def load_model_for_combo(self, model_path=None, device='cpu'):
         if model_path is None:
@@ -280,8 +338,165 @@ class FireFlyGANBase(torch.nn.Module):
         return out_dict
 
 
+class EVAGANBase(torch.nn.Module):
+    def __init__(self, model_path, device=None):
+        super().__init__()
+        if device is None:
+            device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.device = device
+
+        if type(model_path) == dict:
+            '''传入的是config + model'''
+            self.config_path = None
+            self.model_path = None
+            config = dict(model_path['config'])
+            _loaded_state_dict = model_path['model']
+            if str(config['model']) == 'evagan_base_20240808':
+                self.model = EVABase(None, loaded_state_dict=_loaded_state_dict)
+            else:
+                raise ValueError(f" [x] Unknown model: {config['model']}")
+
+        else:
+            # 原始模式
+            self.config_path = os.path.join(os.path.split(model_path)[0], 'config.yaml')
+            self.model_path = model_path
+            config = dict(load_config_dict_from_yaml_path(self.config_path))
+            if str(config["model"]) == 'evagan_base_20240808':
+                self.model = EVABase(self.model_path)
+            else:
+                raise ValueError(f" [x] Unknown model: {config['model']}")
+
+        self.model.eval()
+        self.model.to(self.device)
+        self.sr = config["sampling_rate"]
+        self.hopsize = config["hop_size"]
+        self.dim = config["num_mels"]
+        self.stft = STFT(
+            self.sr,
+            self.dim,
+            config["n_fft"],
+            config["win_size"],
+            config["hop_size"],
+            config["fmin"],
+            config["fmax"]
+        )
+
+    def sample_rate(self):
+        return self.sr
+
+    def hop_size(self):
+        return self.hopsize
+
+    def dimension(self):
+        return self.dim
+
+    def extract(self, audio, keyshift=0):
+        mel = self.stft.get_mel(audio, keyshift=keyshift).transpose(1, 2)  # B, n_frames, bins
+        return mel
+
+    def forward(self, mel, f0=None):
+        with torch.no_grad():
+            c = mel.transpose(1, 2)
+            audio = self.model(c)
+        return audio
+
+    def load_model_for_combo(self, model_path=None, device='cpu'):
+        if model_path is None:
+            model_path = self.model_path
+            assert self.config_path is not None
+        config_path = os.path.join(os.path.split(model_path)[0], 'config.yaml')
+        with open(config_path, "r") as config:
+            config = yaml.safe_load(config)
+        model = torch.load(model_path, map_location=torch.device(device))
+        out_dict = {
+            "config": config,
+            "model": model
+        }
+        return out_dict
+
+
+class EVAGANBig(torch.nn.Module):
+    def __init__(self, model_path, device=None):
+        super().__init__()
+        if device is None:
+            device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.device = device
+
+        if type(model_path) == dict:
+            '''传入的是config + model'''
+            self.config_path = None
+            self.model_path = None
+            config = dict(model_path['config'])
+            _loaded_state_dict = model_path['model']
+            if str(config['model']) == 'evagan_big_20240808':
+                self.model = EVABig(None, loaded_state_dict=_loaded_state_dict)
+            else:
+                raise ValueError(f" [x] Unknown model: {config['model']}")
+
+        else:
+            # 原始模式
+            self.config_path = os.path.join(os.path.split(model_path)[0], 'config.yaml')
+            self.model_path = model_path
+            config = dict(load_config_dict_from_yaml_path(self.config_path))
+            if str(config["model"]) == 'evagan_big_20240808':
+                self.model = EVABig(self.model_path)
+            else:
+                raise ValueError(f" [x] Unknown model: {config['model']}")
+
+        self.model.eval()
+        self.model.to(self.device)
+        self.sr = config["sampling_rate"]
+        self.hopsize = config["hop_size"]
+        self.dim = config["num_mels"]
+        self.stft = STFT(
+            self.sr,
+            self.dim,
+            config["n_fft"],
+            config["win_size"],
+            config["hop_size"],
+            config["fmin"],
+            config["fmax"]
+        )
+
+    def sample_rate(self):
+        return self.sr
+
+    def hop_size(self):
+        return self.hopsize
+
+    def dimension(self):
+        return self.dim
+
+    def extract(self, audio, keyshift=0):
+        mel = self.stft.get_mel(audio, keyshift=keyshift).transpose(1, 2)  # B, n_frames, bins
+        return mel
+
+    def forward(self, mel, f0=None):
+        with torch.no_grad():
+            c = mel.transpose(1, 2)
+            audio = self.model(c)
+        return audio
+
+    def load_model_for_combo(self, model_path=None, device='cpu'):
+        if model_path is None:
+            model_path = self.model_path
+            assert self.config_path is not None
+        config_path = os.path.join(os.path.split(model_path)[0], 'config.yaml')
+        with open(config_path, "r") as config:
+            config = yaml.safe_load(config)
+        model = torch.load(model_path, map_location=torch.device(device))
+        out_dict = {
+            "config": config,
+            "model": model
+        }
+        return out_dict
+
+
+class NothingFlag:
+    pass
+
+
 def load_config_dict_from_yaml_path(path_config):
     with open(path_config, "r") as config:
         args = yaml.safe_load(config)
     return args
-
